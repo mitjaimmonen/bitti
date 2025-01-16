0f8e43009bd7318042e3d2be8e3e869c6bc11c7f
mitja <mitja.immonen@protonmail.com>
2025-01-16 18:38:38 +0200

    feat: add two dimensionally scrollable table for testing

--- lib/application/screens/main_screen/main_screen_journal_shell.dart
+++ lib/application/screens/main_screen/main_screen_journal_shell.dart
@@ -1,5 +1,6 @@
 import 'package:bitti/domain/entities/screen_config_entity.dart';
 import 'package:flutter/material.dart';
+import 'package:two_dimensional_scrollables/two_dimensional_scrollables.dart';
 
 class MainScreenJournalShell extends StatelessWidget {
   const MainScreenJournalShell({super.key});
@@ -12,6 +13,25 @@ class MainScreenJournalShell extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return TableView.builder(
+      diagonalDragBehavior: DiagonalDragBehavior.weightedContinuous,
+      pinnedColumnCount: 1,
+      pinnedRowCount: 1,
+      columnBuilder: (int index) {
+        return const TableSpan(
+          extent: FixedSpanExtent(100.0),
+        );
+      },
+      rowBuilder: (int index) {
+        return const TableSpan(
+          extent: FixedSpanExtent(100.0),
+        );
+      },
+      cellBuilder: (BuildContext context, TableVicinity vicinity) {
+        return TableViewCell(
+          child: Text('Cell ${vicinity.row}, ${vicinity.column}'),
+        );
+      },
+    );
   }
 }
