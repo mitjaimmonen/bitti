d6ea9668f430165090322353d22c6d5223cf2005
mitja <mitja.immonen@protonmail.com>
2025-01-16 18:38:17 +0200

    feat: add blocs

--- lib/application/configuration/injection.dart
+++ lib/application/configuration/injection.dart
@@ -1,5 +1,14 @@
+import 'package:bitti/application/screens/main_screen/bloc_journal/journal_bloc.dart';
+import 'package:bitti/application/screens/main_screen/bloc_notes/notes_bloc.dart';
+import 'package:bitti/application/screens/main_screen/bloc_settings/settings_bloc.dart';
+import 'package:bitti/application/screens/main_screen/bloc_stats/stats_bloc.dart';
 import 'package:get_it/get_it.dart';
 
 final GetIt getIt = GetIt.instance;
 
-Future<void> injectDependencies() async {}
+Future<void> injectDependencies() async {
+  getIt.registerFactory<JournalBloc>(() => JournalBloc());
+  getIt.registerFactory<NotesBloc>(() => NotesBloc());
+  getIt.registerFactory<SettingsBloc>(() => SettingsBloc());
+  getIt.registerFactory<StatsBloc>(() => StatsBloc());
+}
--- lib/application/screens/main_screen/main_screen.dart
+++ lib/application/screens/main_screen/main_screen.dart
@@ -1,5 +1,11 @@
+import 'package:bitti/application/screens/main_screen/bloc_journal/journal_bloc.dart';
+import 'package:bitti/application/screens/main_screen/bloc_notes/notes_bloc.dart';
+import 'package:bitti/application/screens/main_screen/bloc_settings/settings_bloc.dart';
+import 'package:bitti/application/screens/main_screen/bloc_stats/stats_bloc.dart';
 import 'package:bitti/domain/entities/screen_config_entity.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
 import 'package:go_router/go_router.dart';
 
 class MainScreen extends StatelessWidget {
@@ -19,25 +25,32 @@ class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Bitti'),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Theme.of(context).colorScheme.surfaceContainer,
-        selectedItemColor: Theme.of(context).colorScheme.primary,
-        unselectedItemColor: Theme.of(context).colorScheme.secondary,
-        currentIndex: shell.currentIndex,
-        onTap: _onItemTapped,
-        items: shellConfigs
-            .map(
-              (config) => BottomNavigationBarItem(
-                icon: Icon(config.icon),
-                label: config.title,
-              ),
-            )
-            .toList(),
-      ),
-      body: shell,
-    );
+        appBar: AppBar(
+          title: Text(shellConfigs[shell.currentIndex].title),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Theme.of(context).colorScheme.surfaceContainer,
+          selectedItemColor: Theme.of(context).colorScheme.primary,
+          unselectedItemColor: Theme.of(context).colorScheme.secondary,
+          currentIndex: shell.currentIndex,
+          onTap: _onItemTapped,
+          items: shellConfigs
+              .map(
+                (config) => BottomNavigationBarItem(
+                  icon: Icon(config.icon),
+                  label: config.title,
+                ),
+              )
+              .toList(),
+        ),
+        body: MultiBlocProvider(
+          providers: [
+            BlocProvider.value(value: GetIt.I<JournalBloc>()),
+            BlocProvider.value(value: GetIt.I<NotesBloc>()),
+            BlocProvider.value(value: GetIt.I<SettingsBloc>()),
+            BlocProvider.value(value: GetIt.I<StatsBloc>()),
+          ],
+          child: shell,
+        ));
   }
 }
