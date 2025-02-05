3a83c84c2399d7319282caa145394e0aed02bdb5
mitja <mitja.immonen@protonmail.com>
2025-02-05 15:38:31 +0200

    fix: icon asset paths

--- lib/data/mappers/icon_info_data_mapper.dart
+++ lib/data/mappers/icon_info_data_mapper.dart
@@ -2,9 +2,8 @@ import 'package:bitti/data/models/general/asset_icon_info_model.dart';
 import 'package:bitti/domain/enums/icon_name.dart';
 
 class IconInfoDataMapper {
-  static List<AssetIconInfoModel> get iconsInfo {
-    return IconName.values.map((iconName) => getIconInfo(iconName)).toList();
-  }
+  static final List<AssetIconInfoModel> iconsInfo =
+      IconName.values.map((iconName) => getIconInfo(iconName)).toList();
 
   static AssetIconInfoModel getIconInfo(IconName iconName) {
     switch (iconName) {
@@ -91,243 +90,247 @@ class IconInfoDataMapper {
     }
   }
 
+  static String getIconAssetPath(IconName iconName) {
+    return 'assets/icons/icons_${iconName.value}.svg';
+  }
+
   static final homeIcon = AssetIconInfoModel(
     name: IconName.home.value,
-    assetPath: 'assets/icons/home.svg',
+    assetPath: getIconAssetPath(IconName.home),
     tags: ['home', 'house', 'building'],
   );
 
   static final settingsIcon = AssetIconInfoModel(
     name: IconName.settings.value,
-    assetPath: 'assets/icons/settings.svg',
+    assetPath: getIconAssetPath(IconName.settings),
     tags: ['settings', 'gear', 'preferences'],
   );
 
   static final searchIcon = AssetIconInfoModel(
     name: IconName.search.value,
-    assetPath: 'assets/icons/search.svg',
+    assetPath: getIconAssetPath(IconName.search),
     tags: ['search', 'magnifying glass', 'find'],
   );
 
   static final paintingIcon = AssetIconInfoModel(
     name: IconName.painting.value,
-    assetPath: 'assets/icons/painting.svg',
+    assetPath: getIconAssetPath(IconName.painting),
     tags: ['painting', 'art', 'brush'],
   );
 
   static final gardeningIcon = AssetIconInfoModel(
     name: IconName.gardening.value,
-    assetPath: 'assets/icons/gardening.svg',
+    assetPath: getIconAssetPath(IconName.gardening),
     tags: ['gardening', 'plants', 'flowers'],
   );
 
   static final photographyIcon = AssetIconInfoModel(
     name: IconName.photography.value,
-    assetPath: 'assets/icons/photography.svg',
+    assetPath: getIconAssetPath(IconName.photography),
     tags: ['photography', 'camera', 'photo'],
   );
 
   static final burgerIcon = AssetIconInfoModel(
     name: IconName.burger.value,
-    assetPath: 'assets/icons/burger.svg',
+    assetPath: getIconAssetPath(IconName.burger),
     tags: ['burger', 'food', 'fast food'],
   );
 
   static final coffeeIcon = AssetIconInfoModel(
     name: IconName.coffee.value,
-    assetPath: 'assets/icons/coffee.svg',
+    assetPath: getIconAssetPath(IconName.coffee),
     tags: ['coffee', 'drink', 'beverage'],
   );
 
   static final veggiesIcon = AssetIconInfoModel(
     name: IconName.veggies.value,
-    assetPath: 'assets/icons/veggies.svg',
+    assetPath: getIconAssetPath(IconName.veggies),
     tags: ['veggies', 'vegetables', 'food'],
   );
 
   static final soccerIcon = AssetIconInfoModel(
     name: IconName.soccer.value,
-    assetPath: 'assets/icons/soccer.svg',
+    assetPath: getIconAssetPath(IconName.soccer),
     tags: ['soccer', 'football', 'sport'],
   );
 
   static final basketballIcon = AssetIconInfoModel(
     name: IconName.basketball.value,
-    assetPath: 'assets/icons/basketball.svg',
+    assetPath: getIconAssetPath(IconName.basketball),
     tags: ['basketball', 'sport', 'ball'],
   );
 
   static final tennisIcon = AssetIconInfoModel(
     name: IconName.tennis.value,
-    assetPath: 'assets/icons/tennis.svg',
+    assetPath: getIconAssetPath(IconName.tennis),
     tags: ['tennis', 'sport', 'racket'],
   );
 
   static final skiingIcon = AssetIconInfoModel(
     name: IconName.skiing.value,
-    assetPath: 'assets/icons/skiing.svg',
+    assetPath: getIconAssetPath(IconName.skiing),
     tags: ['skiing', 'sport', 'snow'],
   );
 
   static final iceSkatingIcon = AssetIconInfoModel(
     name: IconName.iceSkating.value,
-    assetPath: 'assets/icons/ice_skating.svg',
+    assetPath: getIconAssetPath(IconName.iceSkating),
     tags: ['ice skating', 'sport', 'ice'],
   );
 
   static final skatingIcon = AssetIconInfoModel(
     name: IconName.skating.value,
-    assetPath: 'assets/icons/skating.svg',
+    assetPath: getIconAssetPath(IconName.skating),
     tags: ['skating', 'sport', 'roller'],
   );
 
   static final runningIcon = AssetIconInfoModel(
     name: IconName.running.value,
-    assetPath: 'assets/icons/running.svg',
+    assetPath: getIconAssetPath(IconName.running),
     tags: ['running', 'sport', 'exercise'],
   );
 
   static final notesIcon = AssetIconInfoModel(
     name: IconName.notes.value,
-    assetPath: 'assets/icons/notes.svg',
+    assetPath: getIconAssetPath(IconName.notes),
     tags: ['notes', 'music', 'sound'],
   );
 
   static final guitarIcon = AssetIconInfoModel(
     name: IconName.guitar.value,
-    assetPath: 'assets/icons/guitar.svg',
+    assetPath: getIconAssetPath(IconName.guitar),
     tags: ['guitar', 'music', 'instrument'],
   );
 
   static final pianoIcon = AssetIconInfoModel(
     name: IconName.piano.value,
-    assetPath: 'assets/icons/piano.svg',
+    assetPath: getIconAssetPath(IconName.piano),
     tags: ['piano', 'music', 'instrument'],
   );
 
   static final drumsIcon = AssetIconInfoModel(
     name: IconName.drums.value,
-    assetPath: 'assets/icons/drums.svg',
+    assetPath: getIconAssetPath(IconName.drums),
     tags: ['drums', 'music', 'instrument'],
   );
 
   static final cameraIcon = AssetIconInfoModel(
     name: IconName.camera.value,
-    assetPath: 'assets/icons/camera.svg',
+    assetPath: getIconAssetPath(IconName.camera),
     tags: ['camera', 'photo', 'device'],
   );
 
   static final videoCameraIcon = AssetIconInfoModel(
     name: IconName.videoCamera.value,
-    assetPath: 'assets/icons/video_camera.svg',
+    assetPath: getIconAssetPath(IconName.videoCamera),
     tags: ['video camera', 'film', 'device'],
   );
 
   static final bookIcon = AssetIconInfoModel(
     name: IconName.book.value,
-    assetPath: 'assets/icons/book.svg',
+    assetPath: getIconAssetPath(IconName.book),
     tags: ['book', 'read', 'literature'],
   );
 
   static final beachIcon = AssetIconInfoModel(
     name: IconName.beach.value,
-    assetPath: 'assets/icons/beach.svg',
+    assetPath: getIconAssetPath(IconName.beach),
     tags: ['beach', 'sea', 'sand'],
   );
 
   static final mountainIcon = AssetIconInfoModel(
     name: IconName.mountain.value,
-    assetPath: 'assets/icons/mountain.svg',
+    assetPath: getIconAssetPath(IconName.mountain),
     tags: ['mountain', 'hill', 'nature'],
   );
 
   static final cityIcon = AssetIconInfoModel(
     name: IconName.city.value,
-    assetPath: 'assets/icons/city.svg',
+    assetPath: getIconAssetPath(IconName.city),
     tags: ['city', 'urban', 'buildings'],
   );
 
   static final dogIcon = AssetIconInfoModel(
     name: IconName.dog.value,
-    assetPath: 'assets/icons/dog.svg',
+    assetPath: getIconAssetPath(IconName.dog),
     tags: ['dog', 'animal', 'pet'],
   );
 
   static final catIcon = AssetIconInfoModel(
     name: IconName.cat.value,
-    assetPath: 'assets/icons/cat.svg',
+    assetPath: getIconAssetPath(IconName.cat),
     tags: ['cat', 'animal', 'pet'],
   );
 
   static final birdIcon = AssetIconInfoModel(
     name: IconName.bird.value,
-    assetPath: 'assets/icons/bird.svg',
+    assetPath: getIconAssetPath(IconName.bird),
     tags: ['bird', 'animal', 'flying'],
   );
 
   static final laughIcon = AssetIconInfoModel(
     name: IconName.laugh.value,
-    assetPath: 'assets/icons/laugh.svg',
+    assetPath: getIconAssetPath(IconName.laugh),
     tags: ['laugh', 'emoji', 'happy'],
   );
 
   static final smileIcon = AssetIconInfoModel(
     name: IconName.smile.value,
-    assetPath: 'assets/icons/smile.svg',
+    assetPath: getIconAssetPath(IconName.smile),
     tags: ['smile', 'emoji', 'happy'],
   );
 
   static final sadIcon = AssetIconInfoModel(
     name: IconName.sad.value,
-    assetPath: 'assets/icons/sad.svg',
+    assetPath: getIconAssetPath(IconName.sad),
     tags: ['sad', 'emoji', 'unhappy'],
   );
 
   static final cryIcon = AssetIconInfoModel(
     name: IconName.cry.value,
-    assetPath: 'assets/icons/cry.svg',
+    assetPath: getIconAssetPath(IconName.cry),
     tags: ['cry', 'emoji', 'tears'],
   );
 
   static final starIcon = AssetIconInfoModel(
     name: IconName.star.value,
-    assetPath: 'assets/icons/star.svg',
+    assetPath: getIconAssetPath(IconName.star),
     tags: ['star', 'shape', 'favorite'],
   );
 
   static final heartIcon = AssetIconInfoModel(
     name: IconName.heart.value,
-    assetPath: 'assets/icons/heart.svg',
+    assetPath: getIconAssetPath(IconName.heart),
     tags: ['heart', 'shape', 'love'],
   );
 
   static final checkIcon = AssetIconInfoModel(
     name: IconName.check.value,
-    assetPath: 'assets/icons/check.svg',
+    assetPath: getIconAssetPath(IconName.check),
     tags: ['check', 'mark', 'correct'],
   );
 
   static final crossIcon = AssetIconInfoModel(
     name: IconName.cross.value,
-    assetPath: 'assets/icons/cross.svg',
+    assetPath: getIconAssetPath(IconName.cross),
     tags: ['cross', 'mark', 'wrong'],
   );
 
   static final computerIcon = AssetIconInfoModel(
     name: IconName.computer.value,
-    assetPath: 'assets/icons/computer.svg',
+    assetPath: getIconAssetPath(IconName.computer),
     tags: ['computer', 'device', 'technology'],
   );
 
   static final telephoneIcon = AssetIconInfoModel(
     name: IconName.telephone.value,
-    assetPath: 'assets/icons/telephone.svg',
+    assetPath: getIconAssetPath(IconName.telephone),
     tags: ['telephone', 'device', 'communication'],
   );
 
   static final televisionIcon = AssetIconInfoModel(
     name: IconName.television.value,
-    assetPath: 'assets/icons/television.svg',
+    assetPath: getIconAssetPath(IconName.television),
     tags: ['television', 'device', 'entertainment'],
   );
 }
