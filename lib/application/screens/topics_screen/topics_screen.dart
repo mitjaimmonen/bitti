0b2f3f42565630c8cebdd15c8191e8f014cba87e
mitja <mitja.immonen@protonmail.com>
2026-04-03 10:33:55 +0300

    feat: topics show in topics list

--- lib/application/screens/topics_screen/topics_screen.dart
+++ lib/application/screens/topics_screen/topics_screen.dart
@@ -19,46 +19,76 @@ class TopicsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) => GetIt.I<TopicsBloc>(),
+      create: (context) => GetIt.I<TopicsBloc>()..add(LoadTopicsEvent()),
       child: Scaffold(
         appBar: AppBar(
           title: Text(config.title),
         ),
         body: BlocBuilder<TopicsBloc, TopicsState>(
           builder: (context, state) {
-            return Center(
-              child: Column(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'No topics found',
-                    style: Theme.of(context).textTheme.headlineSmall,
-                  ),
-                  const SizedBox(height: 32.0),
-                  SketchButtonHeadline(
-                    text: 'Add Topic',
-                    onPressed: () async {
-                      final data =
-                          await context.push<TopicEditorReturnViewModel>(
-                        TopicEditorScreen.config.routePath,
-                        extra: const TopicEditorExtraViewModel(),
-                      );
-
-                      if (!context.mounted) return;
-
-                      if (data?.topicEntry != null) {
-                        final bloc = context.read<TopicsBloc>();
-                        bloc.add(AddTopicEvent(topic: data!.topicEntry!));
-                      }
-                    },
-                  ),
-                ],
-              ),
-            );
+            switch (state) {
+              case TopicsInitial():
+              case TopicsLoading():
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              case TopicsErrored():
+                return Center(
+                  child: Text('Something went wrong'),
+                );
+              case TopicsLoaded():
+                if (state.topics?.isEmpty ?? true) {
+                  return _buildNoTopicsView(context);
+                } else {
+                  return _buildTopicsListView(context, state);
+                }
+            }
           },
         ),
       ),
+    );
+  }
+
+  Widget _buildNoTopicsView(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'No topics found',
+            style: Theme.of(context).textTheme.headlineSmall,
+          ),
+          const SizedBox(height: 32.0),
+          SketchButtonHeadline(
+            text: 'Add Topic',
+            onPressed: () async {
+              final data = await context.push<TopicEditorReturnViewModel>(
+                TopicEditorScreen.config.routePath,
+                extra: const TopicEditorExtraViewModel(),
+              );
+
+              if (!context.mounted) return;
+
+              if (data?.topicEntry != null) {
+                final bloc = context.read<TopicsBloc>();
+                bloc.add(AddTopicEvent(topic: data!.topicEntry!));
+              }
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTopicsListView(BuildContext context, TopicsLoaded state) {
+    return ListView.builder(
+      itemCount: state.topics?.length ?? 0,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(state.topics![index].name),
+        );
+      },
     );
   }
 }
